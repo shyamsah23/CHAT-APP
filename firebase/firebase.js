cc6a52d9b68f648a80c4b2fffaabb7b8b3fec618
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2023-07-13 10:26:49 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDj-HkP2fZPyZ1IjiBFOZLp4F19JaBZQEU",
-  authDomain: "fir-chatapp-78112.firebaseapp.com",
-  projectId: "fir-chatapp-78112",
-  storageBucket: "fir-chatapp-78112.appspot.com",
-  messagingSenderId: "831686543880",
-  appId: "1:831686543880:web:4b4872ec27357013d81fbd"
+  apiKey: "Enter your Own Key",
+  authDomain: "Enter your Own Key",
+  projectId: "Enter your Own Key",
+  storageBucket: "Enter your Own Key",
+  messagingSenderId: "Enter your Own Key",
+  appId: "Enter your Own Key"
 };
 
 
